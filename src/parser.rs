7a35f486b855619bb10a97b5486cca424e9fc8fa
Jachdich <jamesedkitching@gmail.com>
2023-07-06 13:59:27 +0100

    ?

--- src/parser.rs
+++ src/parser.rs
@@ -6,28 +6,28 @@ use crate::{ListEntry, ListItem, TodoList};
 
 struct ParseError(String);
 
-fn parse_str(s: &str) -> Result<Vec<TodoList>, ParseError> {
-    let res: Vec<TodoList> = Vec::new();
-    let mut lines = s.lines().enumerate();
-    while let Some((line_num, mut item_name)) = lines.next() {
-        if item_name.trim().is_empty() {
-            // skip empty lines
-            continue;
-            if first_char.is_none() {
-                continue;
-            }
-        }
-        let first_char = item_name.chars().next().unwrap(); // unwrap OK because we just checked that the string wasn't empty
-        let item_name = item_name.trim_end();
-        if !item_name.ends_with(':') {
-            return Err(ParseError(format!(
-                "Expected ':' at end of list definition (line {})",
-                line_num + 1
-            )));
-        }
-    }
-    Ok(res)
-}
+// fn parse_str(s: &str) -> Result<Vec<TodoList>, ParseError> {
+//     let res: Vec<TodoList> = Vec::new();
+//     let mut lines = s.lines().enumerate();
+//     while let Some((line_num, mut item_name)) = lines.next() {
+//         if item_name.trim().is_empty() {
+//             // skip empty lines
+//             continue;
+//             if first_char.is_none() {
+//                 continue;
+//             }
+//         }
+//         let first_char = item_name.chars().next().unwrap(); // unwrap OK because we just checked that the string wasn't empty
+//         let item_name = item_name.trim_end();
+//         if !item_name.ends_with(':') {
+//             return Err(ParseError(format!(
+//                 "Expected ':' at end of list definition (line {})",
+//                 line_num + 1
+//             )));
+//         }
+//     }
+//     Ok(res)
+// }
 
 // #[cfg(test)]
 // mod tests {
